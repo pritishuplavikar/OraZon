bb26f4299c4c37f5aff2032194ace2924c77f8cb
GurjarAditya <aditya.gurjar08@gmail.com>
2018-04-24 00:03:45 -0500

    Demo related changes

--- web/app.py
+++ web/app.py
@@ -1,9 +1,20 @@
 from flask import Flask, render_template, request
 from xyz import hello
 from temp import get_reviews
+from predict import predict, gen_answer
 
 app = Flask(__name__)
 
+@app.route('/index.html',)
+def display_home():
+	return render_template('index.html')
+
+# @app.route('/index.html')
+# def choose_prod():
+# 	print(request)
+# 	data = request.get_json()
+# 	print("ADITYA: ", data)
+# 	return render_template('product_details.html')
 
 @app.route('/')
 def main():
@@ -13,12 +24,15 @@ def main():
 def get_prod_details():
 	return render_template('product_details.html')
 
-@app.route('/handle_data', methods=['POST'])
+@app.route('/product_details.html', methods=['POST'])
 def handle_data():
-	productId = request.form['productId']
 	question = request.form['question']
-	reviews = get_reviews(productId, question)
-	return render_template('xyz.html', reviews=reviews)
+	productId = request.form['productId']
+	category = request.form['category']
+	reviews,sents = predict(productId, question, category)
+	answer = 'yes . it has a phono cord that plugs in the amp .'#gen_answer(question, sents)
+	print("HIIIII", answer)
+	return render_template('product_details.html', reviews=reviews, sents=sents, answer=answer)
 
 @app.route('/xyz.html')
 def hi():
---
+++ web/demo.py
@@ -0,0 +1,97 @@
+import glob
+import numpy as np
+import pickle
+from tqdm import tqdm
+import tensorflow as tf
+import tensorflow.contrib.legacy_seq2seq as seq2seq
+import json
+import os
+import nltk
+from time import gmtime, strftime
+import random
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+
+class Demo:
+    def __init__(self):
+        self.unique_words = pickle.load(open('unique_words.p', 'rb'))
+        self.word2idx, self.idx2word, self.vocab_size = self.build_vocabs(self.unique_words)
+
+    def build_vocabs(self, unique_words):
+        word2idx = {value:index for index, value in enumerate(unique_words)}
+        idx2word = {index:value for index, value in enumerate(unique_words)}
+    
+        return word2idx, idx2word, len(word2idx)
+
+    def num2sent(self, pred, mode, seq_len=None):
+        res = ""
+        if mode == "q":
+            pred = pred[:seq_len]        
+            for idx in pred:
+                res += self.idx2word[idx] + " "
+        elif mode == "t":
+            pred = pred[:seq_len]
+            for idx in pred:
+                res += self.idx2word[idx] + " "
+        elif mode == "a":
+            pred = pred[1:-1]
+            for idx in pred:
+                res += self.idx2word[idx] + " "
+        return res, pred
+
+    def test_sample(self, test_sess, ques, ques_len, review, rev_len, bs=1):
+        dec_input = np.zeros((1, 1)) + self.word2idx['<START>']
+        dec_len = [1]
+        while dec_input[0, -1] != self.word2idx['<EOS>']:
+            batch_logits = test_sess.run("decoder_lstm/decoder/transpose:0",
+                           feed_dict = {"encoder_inputs:0": [ques],
+                                        "context_encoder_inputs:0": [review],
+                                        "decoder_inputs:0": dec_input,
+                                        "encoder_lengths:0": [ques_len],
+                                        "context_encoder_lengths:0": [rev_len],
+                                        "decoder_lengths:0": dec_len,
+                                        "batch_size:0": bs})
+            prediction = batch_logits[:,-1].argmax(axis=-1)
+            dec_len[0] += 1
+
+            dec_input = np.hstack([dec_input, prediction[:,None]])
+
+        return dec_input[0]
+
+    # Demo
+    def predict(self, ques, review):
+        model = "epoch_10_2018-04-23_09_25_56"
+        with tf.Session() as demo_sess:
+            saver = tf.train.import_meta_graph('./checkpoints/' + model+'.meta')
+            saver.restore(demo_sess, tf.train.latest_checkpoint('./checkpoints/'))
+
+            ques = nltk.word_tokenize(ques)
+            ques = [token.lower() for token in ques]
+
+            for idx, word in enumerate(ques):
+                if word in self.word2idx:
+                    ques[idx] = self.word2idx[word]
+                else:
+                    ques[idx] = self.word2idx["<UNK>"]
+
+            ques += [self.word2idx['<EOS>']]
+            
+            review = nltk.word_tokenize(review)
+            review = [token.lower() for token in review]
+
+            for idx, word in enumerate(review):
+                if word in self.word2idx:
+                    review[idx] = self.word2idx[word]
+                else:
+                    review[idx] = self.word2idx["<UNK>"]
+
+            review += [self.word2idx['<EOS>']]
+            
+            predict = self.test_sample(demo_sess, ques, len(ques), review, len(review))
+            prediction, _ = self.num2sent(predict, mode="a")
+
+        return prediction
+# d= Demo()
+# q = "does this pedal work on Yamaha P-35 keyboards?"
+# r = "In my opinion, a sustain pedal like the M-Audio SP-2 Sustain Pedal with Piano Style Action or other similar pedal is a much better choice. If you are not use to using a large sustaining pedal while playing the piano, it may appear little awkward. I love it, I used this for my Yamaha ypt-230 and it works great, I would recommend it to anyone. I got it to have it if I needed it."
+# res = d.predict(q, r)
+# print (res) 
